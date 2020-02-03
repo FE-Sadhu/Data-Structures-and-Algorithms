85f288f6b1a43b8c69b926a83e71338e4cadc712
sadhu <1714222385@qq.com>
2020-02-03 17:12:22 +0800

    优先队列

--- src/core/Queue.js
+++ src/core/Queue.js
@@ -5,7 +5,7 @@
  */
 const items = new WeakMap()
 
-class Queue {
+export class Queue {
   constructor() {
     items.set(this, [])
   }
@@ -21,6 +21,61 @@ class Queue {
   isEmpty() {
     return items.get(this).length === 0
   }
+  size() {
+    return items.get(this).length
+  }
+}
+
+/**
+ * 最小优先队列
+ * (类似急诊室先让更严重的病人入队列一样)
+ * 定个优先级就行，在队列里，优先级高的(priority 值小的)排在前面
+ */
+
+class QueueElement { // 添加优先级,生成特殊元素,是特殊元素入队列
+  constructor(element, priority) {
+    this.element = element
+    this.priority = priority
+  }
+}
+
+const items2 = new WeakMap()
+export class PriorityQueue {
+  constructor() {
+    items2.set(this, [])
+  }
+  enqueue(val, priority) {
+    let priorityElement = new QueueElement(val, priority) // 生成带优先级的特殊元素
+    const queue = items2.get(this)
+    let added = false
+
+    for(let i = 0; i < queue.length; i++) {
+      if (priorityElement.priority < queue[i].priority) {
+        queue.splice(i, 0, priorityElement)
+        added = true
+        break
+      }
+    }
+
+    if (!added) {
+      queue.push(priorityElement)
+    }
+
+  }
+
+  dequeue() {
+    return items2.get(this).shift()
+  }
+
+  front() {
+    let first = items2.get(this)[0]
+    return `${first.element} - ${first.priority}`
+  }
+
+  isEmpty() {
+    return items.get(this).length === 0
+  }
+  
   size() {
     return items.get(this).length
   }
