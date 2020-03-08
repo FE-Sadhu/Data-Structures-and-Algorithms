51fe1cc25695f4a6c5f6ba859309973716642355
sadhu <1714222385@qq.com>
2020-03-08 22:00:22 +0800

    lc 1 优化

--- src/exercises/顺序刷题/1.两数之和.js
+++ src/exercises/顺序刷题/1.两数之和.js
@@ -12,13 +12,26 @@
  */
 var twoSum = function(nums, target) {
   // 暴力
-  for (let i = 0; i < nums.length; i++) {
-    for(let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
+  // for (let i = 0; i < nums.length; i++) {
+  //   for(let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] === target) {
+  //       return [i, j];
+  //     }
+  //   }
+  // }
+  // 时间复杂度： O(n^2)，空间复杂度: O(1);
+
+  // 哈希表
+  const cache = {};
+  for(let i = 0; i < nums.length; i++) {
+    const rest = target - nums[i];
+    if (rest in cache) {
+      return [i, cache[rest]]
     }
+    cache[nums[i]] = i;
   }
+  // 空间换时间，只有一层遍历
+  // O(n), O(n)
 };
 // @lc code=end
 
