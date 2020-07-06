a857bb7b7cadf262ef6770147d52e2d3595d2d44
sadhu <1714222385@qq.com>
2020-07-06 20:01:19 +0800

    backtracking lc 51

---
+++ src/exercises/顺序刷题/51.n皇后.js
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=javascript
+ *
+ * [51] N皇后
+ *
+ * https://leetcode-cn.com/problems/n-queens/description/
+ *
+ * algorithms
+ * Hard (68.19%)
+ * Likes:    455
+ * Dislikes: 0
+ * Total Accepted:    47.9K
+ * Total Submissions: 68.3K
+ * Testcase Example:  '4'
+ *
+ * n 皇后问题研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 
+ * 
+ * 
+ * 上图为 8 皇后问题的一种解法。
+ * 
+ * 给定一个整数 n，返回所有不同的 n 皇后问题的解决方案。
+ * 
+ * 每一种解法包含一个明确的 n 皇后问题的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ * 
+ * 示例:
+ * 
+ * 输入: 4
+ * 输出: [
+ * ⁠[".Q..",  // 解法 1
+ * ⁠ "...Q",
+ * ⁠ "Q...",
+ * ⁠ "..Q."],
+ * 
+ * ⁠["..Q.",  // 解法 2
+ * ⁠ "Q...",
+ * ⁠ "...Q",
+ * ⁠ ".Q.."]
+ * ]
+ * 解释: 4 皇后问题存在两个不同的解法。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 
+ * 皇后，是国际象棋中的棋子，意味着国王的妻子。皇后只做一件事，那就是“吃子”。当她遇见可以吃的棋子时，就迅速冲上去吃掉棋子。当然，她横、竖、斜都可走一到七步，可进可退。（引用自
+ * 百度百科 - 皇后 ）
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function(n) {
+  const board = new Array(n).fill('.'.repeat(n)).map(item => item.split('')),
+        result = [];
+
+  function isValid(board, row, col) {
+    // 检查列
+    for (let i = 0; i < n; i++) {
+      if (board[i][col] === 'Q') {
+        return false;
+      }
+    }
+
+    // 检查左上方
+    for(let r = row - 1, c = col - 1; r >= 0 && c >= 0; r--, c--) {
+      if (board[r][c] === 'Q') {
+        return false;
+      }
+    }
+
+    // 检查右上方
+    for (let r = row - 1, c = col + 1; r >= 0 && c < n ; r--, c++) {
+      if (board[r][c] === 'Q') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function backTrack(board, row) {
+    if (row === n) {
+      result.push(board.map(item => item.join('')));
+      return;
+    }
+    let colLen = board[row].length;
+    for(let col = 0; col < colLen; col++) {
+      if (!isValid(board, row, col)) {
+        continue;
+      }
+      board[row].splice(col, 1, 'Q');
+      backTrack(board, row + 1);
+      board[row].splice(col, 1, '.');
+    }
+  }
+
+  backTrack(board, 0);
+  return result;
+};
+// @lc code=end
+
