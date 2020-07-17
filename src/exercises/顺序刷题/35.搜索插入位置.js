8635ad85e214dcf2b7bc841990f2523453845d60
sadhu <1714222385@qq.com>
2020-07-17 18:40:21 +0800

    【二分查找】 lc 35

---
+++ src/exercises/顺序刷题/35.搜索插入位置.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode.cn id=35 lang=javascript
+ *
+ * [35] 搜索插入位置
+ *
+ * https://leetcode-cn.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (45.26%)
+ * Likes:    597
+ * Dislikes: 0
+ * Total Accepted:    204.8K
+ * Total Submissions: 440.2K
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 
+ * 你可以假设数组中无重复元素。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,3,5,6], 5
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [1,3,5,6], 2
+ * 输出: 1
+ * 
+ * 
+ * 示例 3:
+ * 
+ * 输入: [1,3,5,6], 7
+ * 输出: 4
+ * 
+ * 
+ * 示例 4:
+ * 
+ * 输入: [1,3,5,6], 0
+ * 输出: 0
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let left = 0,
+      right = nums.length - 1;
+  
+  while(left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if(target > nums[mid]) {
+      left = mid + 1;
+    } else if(target < nums[mid]) {
+      right = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return Math.max(left, right);
+};
+// @lc code=end
+
