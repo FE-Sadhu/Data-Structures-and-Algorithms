a99f8085b23cee9696b4f9e9f2feba881078e14f
sadhu <1714222385@qq.com>
2020-07-18 23:00:54 +0800

    【滑动窗口】 lc 567

--- src/exercises/顺序刷题/567.字符串的排列.js
+++ src/exercises/顺序刷题/567.字符串的排列.js
@@ -52,7 +52,42 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
+  // 方法一： 滑动窗口。 还有种不推荐的方法是回溯
+  let left = 0,
+      right = 0,
+      count = 0; // 窗口中符合目标元素要求的个数
+  
+  const target = {},
+        window = {}; // 窗口中包含的目标元素以及个数
+  for(let val of s1) {
+    target[val] = target[val] ? ++target[val] : 1;
+  }
 
+  while(right < s2.length) {
+    let add = s2[right]; // 待添加进 window
+    right++; // 添加进去
+    if(add in target) {
+      window[add] = window[add] ? ++window[add] : 1;
+      if(window[add] === target[add]) {
+        count++;
+      }
+    }
+    // 什么时机缩小窗口
+    while(right - left >= s1.length) {
+      if(count === Object.keys(target).length) return true;
+      let d = s2[left];
+      left++;
+      if(d in target) {
+        if(window[d] === target[d]) {
+          count--;
+        }
+        window[d] = --window[d];
+      }
+    }
+
+  }
+
+  return false;
 };
 // @lc code=end
 
