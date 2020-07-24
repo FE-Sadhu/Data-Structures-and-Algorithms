c656626a5617af60a3e9a80e3bfd598c1723c32e
sadhu <1714222385@qq.com>
2020-07-24 18:26:02 +0800

    【DFS】 lc 113

---
+++ src/exercises/顺序刷题/113.路径总和-ii.js
@@ -0,0 +1,79 @@
+/*
+ * @lc app=leetcode.cn id=113 lang=javascript
+ *
+ * [113] 路径总和 II
+ *
+ * https://leetcode-cn.com/problems/path-sum-ii/description/
+ *
+ * algorithms
+ * Medium (58.99%)
+ * Likes:    279
+ * Dislikes: 0
+ * Total Accepted:    61.4K
+ * Total Submissions: 101.4K
+ * Testcase Example:  '[5,4,8,11,null,13,4,7,2,null,null,5,1]\n22'
+ *
+ * 给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例:
+ * 给定如下二叉树，以及目标和 sum = 22，
+ * 
+ * ⁠             5
+ * ⁠            / \
+ * ⁠           4   8
+ * ⁠          /   / \
+ * ⁠         11  13  4
+ * ⁠        /  \    / \
+ * ⁠       7    2  5   1
+ * 
+ * 
+ * 返回:
+ * 
+ * [
+ * ⁠  [5,4,11,2],
+ * ⁠  [5,8,4,5]
+ * ]
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {number[][]}
+ */
+var pathSum = function(root, sum) {
+  if(!root) return [];
+  const res = [];
+
+  function dfs (road, node) {
+    if(!node.left && !node.right) {
+      if(sum === road.reduce((a, b) => a + b)) {
+        res.push(road.slice());
+      }
+      return;
+    }
+
+    let chooses = [node.left, node.right];
+    for(let choose of chooses) {
+      if(!choose) continue;
+      road.push(choose.val);
+      dfs(road, choose);
+      road.pop();
+    }
+  }
+  dfs([root.val], root);
+  return res;
+};
+// @lc code=end
+
