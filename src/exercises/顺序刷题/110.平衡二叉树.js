291410ab4139b9fb24326164d3e550995393326e
sadhu <1714222385@qq.com>
2020-07-24 16:32:33 +0800

    【二叉树】 lc 110

---
+++ src/exercises/顺序刷题/110.平衡二叉树.js
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=javascript
+ *
+ * [110] 平衡二叉树
+ *
+ * https://leetcode-cn.com/problems/balanced-binary-tree/description/
+ *
+ * algorithms
+ * Easy (50.59%)
+ * Likes:    380
+ * Dislikes: 0
+ * Total Accepted:    92.9K
+ * Total Submissions: 176.9K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 
+ * 
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ * 
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ * 
+ * 返回 true 。
+ * 
+ * 示例 2:
+ * 
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ * 
+ * ⁠      1
+ * ⁠     / \
+ * ⁠    2   2
+ * ⁠   / \
+ * ⁠  3   3
+ * ⁠ / \
+ * ⁠4   4
+ * 
+ * 
+ * 返回 false 。
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+  // 递归: 自底向上
+  // 定义: 给定一个节点，求这个节点为根节点时的高度
+  function helper(node) {
+    if(node === null) {
+      return 0
+    };
+
+    let l = helper(node.left);
+    let r = helper(node.right);
+    if(l === -1 || r === -1 || Math.abs(l - r) > 1) return -1;
+    return Math.max(l, r) + 1; // 加上自身的高度 1
+  }
+  return helper(root) === -1 ? false : true;
+};
+// @lc code=end
+
