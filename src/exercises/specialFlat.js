8cd217e73fdb788754c7777bca292458d57f0aad
sadhu <1714222385@qq.com>
2020-07-25 20:55:48 +0800

    【编程题】优化 进阶的特殊去重

--- src/exercises/specialFlat.js
+++ src/exercises/specialFlat.js
@@ -13,40 +13,85 @@
  *		d: [1, [2, 3, [4]]],      		==> 	d: [1, 2, 3, 4]
  *		e: 1									e: 1
  *	})										}
+ *
+ * {
+ * 	a: {
+ * 		b: 1,
+ * 		c: {
+ * 			d: [1, 2, [3, { e: {g : 2}}]],
+ * 			f: 'f'
+ * 			}
+ * 		}
+ * }
+ * ===>
+ * 
+ * {
+ * a.b: 1,
+ * a.c.d: [1, 2, 3, {e.g: 2}],
+ * a.c.f: 'f'
+ * }
+ * 
  */
 
 function flat(obj) {
-	// deal array
-	function dealArr(arr) {
-	    return arr.reduce(function(prev, next){
-	        return prev.concat(Array.isArray(next) ? dealArr(next) : next)
-	    }, [])
-	}
-	// deal object
-	function dealObj(obj) {
-		for(let key in obj) {
-			let val = obj[key];
-			return [`.${key}`].concat((Object.prototype.toString.call(val) === '[object Object]' ? dealObj(val) : Array.isArray(val) ? [dealArr(val)] : val));
-		}
-	}
-	// deal process 
-	for(let key in obj) {
-		let val = obj[key];
 
-		if(Object.prototype.toString.call(val) === '[object Object]') { // deal object
-			const dealed = dealObj(val);
-			let value = dealed.pop();
-			obj[key + dealed.join('')] = value
-			delete obj[key];
-		} else if(Array.isArray(val)) { // deal array
-			obj[key] = dealArr(val);
-		} else { // the others
-			obj[key] = val;
-		}
+	function isArray(arr) {
+		return Array.isArray(arr);
 	}
-	return obj;
+
+	function isObject(obj) {
+		return Object.prototype.toString.call(obj) === '[object Object]';
+	}
+
+	function flattenArr(arr) {
+		return arr.reduce((prev, next) => {
+			return prev.concat(Array.isArray(next) ? flattenArr(next) : isObject(next) ? flattenObj(next) : next);
+		}, [])
+	}
+
+	function flattenObj(obj, key = '', res = {}) {
+		for (let [k, v] of Object.entries(obj)) {
+			if (isObject(v)) {
+				let tmp = key + k + '.'
+				flattenObj(v, tmp, res); // 这个递归仅传递 res 和 key
+			} else {
+				let tmp = key + k;
+				res[tmp] = isArray(v) ? flattenArr(v) : v;
+			}
+		}
+		return res;
+	}
+
+	return flattenObj(obj);
 }
 
+let res = flat({
+	a: {
+		b: 1,
+		c: {
+			d: [1, 2, [3, {
+				e: {
+					g: 2
+				}
+			}]],
+			f: 'f'
+		}
+	},
+	b: [1, 2, [3]],
+	e: 1
+})
+console.log(res); 
+/*
+{
+	'a.b': 1,
+	'a.c.d': [1, 2, 3, {
+		'e.g': 2
+	}],
+	'a.c.f': 'f',
+	b: [1, 2, 3],
+	e: 1
+}
+*/
 /*
 function createIterator(arr) {
 	let count = 0;
