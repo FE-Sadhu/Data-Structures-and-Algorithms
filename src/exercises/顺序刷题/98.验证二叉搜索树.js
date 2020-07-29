dd807acb9059a012e7c9db339d1c85fc586e685b
sadhu <1714222385@qq.com>
2020-07-29 22:52:09 +0800

    【BST】 lc 98

---
+++ src/exercises/顺序刷题/98.验证二叉搜索树.js
@@ -0,0 +1,71 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=javascript
+ *
+ * [98] 验证二叉搜索树
+ *
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (29.21%)
+ * Likes:    684
+ * Dislikes: 0
+ * Total Accepted:    148.2K
+ * Total Submissions: 464.4K
+ * Testcase Example:  '[2,1,3]'
+ *
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * 
+ * 假设一个二叉搜索树具有如下特征：
+ * 
+ * 
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 
+ * 
+ * 示例 1:
+ * 
+ * 输入:
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * 输出: true
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入:
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ * 根节点的值为 5 ，但是其右子节点值为 4 。
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  function helper(root, min, max) {
+    if(!root) return true; // 空节点也算 BST
+    if(root.val <= min || root.val >= max) return false;
+    return helper(root.left, min, root.val) && helper(root.right, root.val, max)
+  }
+  return helper(root, -Infinity, +Infinity);
+};
+// @lc code=end
+
